d384f46697f50f8fcf2306131347ea97fd3bc7de
nawazish-qaavi78 <123806130+nawazish-qaavi78@users.noreply.github.com>
2023-11-10 21:31:11 +0530

    Add files via upload
    enabling multi-language

---
+++ v2.py
@@ -0,0 +1,78 @@
+import speech_recognition as sr
+import pyrebase
+import langcodes
+
+config = {
+  "apiKey": "AIzaSyClastSM-d8sm0AWajY03OnezmkPVCrO04",
+  "authDomain": "fir-topythonsample.firebaseapp.com",
+  "projectId": "fir-topythonsample",
+  "storageBucket": "fir-topythonsample.appspot.com",
+  "databaseURL":"https://fir-topythonsample-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  "messagingSenderId": "936536554705",
+  "appId": "1:936536554705:web:f4d0b91b36e8174d7e99f0",
+  "measurementId": "G-JL324VD9EC"
+}
+
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
+
+
+recognizer = sr.Recognizer()
+
+
+def find_user(text):
+    users = database.child("Users").order_by_child("Text").equal_to(text.lower()).get()
+    if not len(users.each()):  
+            print("User not found")  ###
+    else:
+        for user in users.each():
+            if (user.val()!=""):
+                print("Welcome, {}" .format(user.val()["Name"]))   
+
+def re_run_program(count):
+    if(count<5):
+        print(f"Recording {count+1}")
+        record(count+1)
+    else:
+        print("Your presence undetected")  ##
+        
+def recognize_audio(recorded_audio, lang_code):
+    try:
+        print("Recognizing the text")
+        text = recognizer.recognize_google(
+                recorded_audio, 
+                language=lang_code
+        )
+
+        print("Decoded Text : {}".format(text))
+        return text 
+    except sr.exceptions.UnknownValueError:
+        print("Couldn't comprehend. Please speak clearly")
+       
+    except Exception as ex:
+        print(ex)
+        
+    
+
+''' recording the sound '''
+
+def record(lang_code):
+    
+    with sr.Microphone() as source:
+        print("Adjusting noise ")
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+        print("Recording for 4 seconds")
+        try:
+            recorded_audio = recognizer.listen(source, timeout=4) 
+            print("Done recording")
+            return recognize_audio(recorded_audio, lang_code)
+               
+        except sr.exceptions.WaitTimeoutError:
+            print("Recording Time out. Please speak while recording")
+              
+        except Exception as ex:
+            print(ex)
+        
+lang = record("en-US") 
+code_word = record(langcodes.find(lang).language)
+find_user(code_word)
