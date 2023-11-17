951761422798950c343f1594df96660876cf2122
nawazish-qaavi78 <123806130+nawazish-qaavi78@users.noreply.github.com>
2023-11-17 10:48:37 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,6 +1,16 @@
 from speech import record
 from module import main
 
+# def final(count):
+#     stage1_result = record(count)
+#     print(stage1_result)
+#     # if(stage1_result[0]):
+#     #     stage2_result=main(stage1_result[1])
+#     #     if not stage2_result:
+#     #         final(stage1_result[1]+1)
+    
+# final(0)
+
 stage1_result = record(0)
 if(stage1_result):
-    main()
+    main(0)
